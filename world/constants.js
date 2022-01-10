b7218572a7961183d675f7a3c823a6d123ca3e18
Alessandro Solbiati <lessandro@fb.com>
2022-01-10 15:01:49 +0000

    [world] resized world so no need to control camera (camera broken?)

--- world/constants.js
+++ world/constants.js
@@ -2,10 +2,10 @@
  * Created by Filip on 2018-12-02.
  */
 let TILESIZE = 32;
-let GRID_MIN_X = -200;
-let GRID_MIN_Y = -200;
-let GRID_MAX_X = 1000;
-let GRID_MAX_Y = 1000;
+let GRID_MIN_X = 0;
+let GRID_MIN_Y = 0;
+let GRID_MAX_X = 1500;
+let GRID_MAX_Y = 800;
 let FACTORIES = 2;
 let MINES = 2;
 
